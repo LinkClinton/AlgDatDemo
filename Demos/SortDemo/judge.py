fadfcbc0cc2758fcdb8ed242375a7e07af86dbc9
Link Clinton <LinkClinton@outlook.com>
2020-04-10 13:41:32 +0800

    opt performance.

--- Demos/SortDemo/judge.py
+++ Demos/SortDemo/judge.py
@@ -1,6 +1,6 @@
 import os;
 
-for i in range(0, 5):
+for i in range(0, 100):
 	os.system("./disk_gen 100000 > input_data")
 	os.system("./a.out input_data 8 1024 output")
 	os.system("./ans_build < input_data > answer")
--- Demos/SortDemo/main.cpp
+++ Demos/SortDemo/main.cpp
@@ -43,11 +43,13 @@ void merge_sort(virtual_memory<element>& memory, memory_usage& usage, size_t lhs
 	// we will divide the memory into k group memory
 	const auto group_size = (size % k == 0) ? (size / k) : ((size / k) + 1);
 
+	std::vector<size_t> group_offset(k);
 	std::vector<size_t> group_begin(k);
 	std::vector<size_t> group_end(k);
 
 	// sort groups
 	for (size_t index = 0; index < k; index++) {
+		group_offset[index] = std::min(index * group_size + lhs, rhs);
 		group_begin[index] = std::min(index * group_size + lhs, rhs);
 		group_end[index] = std::min((index + 1) * group_size + lhs, rhs);
 
@@ -55,69 +57,62 @@ void merge_sort(virtual_memory<element>& memory, memory_usage& usage, size_t lhs
 	}
 
 	std::vector<element> init_values(k);
+	std::vector<virtual_memory<element>> temp_memories;
 
+	// we divide the memory into k groups, to store the groups data in disk.
+	// each memory has (memory_size / k) element to cache the group data in disk
+	for (size_t index = 0; index < k; index++) {
+		auto memory_size = (memory.memory_size() % k == 0) ? memory.memory_size() / k : memory.memory_size() / k + 1;
+		auto temp_memory = virtual_memory<element>(group_end[index] - group_begin[index], memory_size);
+
+		// copy the group data from main memory to temp memory.
+		for (size_t it = group_begin[index]; it < group_end[index]; it++) 
+			temp_memory.write_with_cache(it - group_begin[index], memory.read_with_cache(it));
+
+		temp_memory.write_back();
+		
+		temp_memories.push_back(temp_memory);
+	}
+	
 	for (size_t index = 0; index < k; index++) {
 		// when the group is empty, we set the max value
-		if (group_begin[index] == group_end[index]) {
+		if (group_offset[index] == group_end[index]) {
 			init_values[index] = std::numeric_limits<element>::max();
 
 			continue;
 		}
 
-		// read the first element in group
-		// we use only use one cache, so when we change the group we need read a new block
-		// and we do not need write block, because we do not change anything
-		memory.read_block(group_begin[index]);
-		
-		init_values[index] = memory[group_begin[index]++];
+		// read the first element in group	
+		init_values[index] = temp_memories[index].read_with_cache(0);
+
+		group_offset[index]++;
 	}
 
 	// build competition tree to sort groups
 	competition_tree<element> tree(init_values);
 		
-	// because we can not modify the memory before we finished to sort the groups
-	// we will use a temp memory to store the result
-	virtual_memory<element> temp_memory(size, memory.memory_size());
-
+	// because we store the memory to temp memories, so we can modify the main memory.
 	for (size_t index = 0; index < size; index++) {
-		const auto end = std::min(temp_memory.position() + temp_memory.memory_size(), temp_memory.disk_size());
 		const auto group_id = tree.top_id();
+
+		memory.write_with_cache(lhs + index, tree.top_value());
 		
-		// when the index is out of block, we need write block to disk and read a new block from disk
-		if (index < temp_memory.position() || index >= end) {
-			temp_memory.write_back();
-			
-			temp_memory.read_block(index);
-		}
-
-		temp_memory[index] = tree.top_value();
-
 		// when the group is not empty, we will read it from memory
 		// virtual_memory::read_with_cache() will read data from cache
 		// when the data is not in the cache, we will read it from disk
 		// notice : the old block will not write back when we read a new block
-		if (group_begin[group_id] != group_end[group_id])
-			tree.replay(memory.read_with_cache(group_begin[group_id]++));
+		if (group_offset[group_id] != group_end[group_id])
+			tree.replay(temp_memories[group_id].read_with_cache((group_offset[group_id]++) - group_begin[group_id]));
 		else
 			tree.replay();
 	}
 
 	// write the last block to disk
-	temp_memory.write_back();
-
-	// now, copy the temp_memory to memory
-	// virtual_memory::write_with_cache() will write data to cache
-	// when the data is not in the cache, we will read it from disk
-	// notice : the old block will write back when we read a new block
-	for (size_t index = 0; index < size; index++) {
-		memory.write_with_cache(lhs + index, temp_memory.read_with_cache(index));
-	}
-
-	// write the last block to disk
 	memory.write_back();
-	
+
 	// we also need record the usage of temp memory
-	usage = usage + temp_memory.usage();
+	for (const auto& temp_memory : temp_memories)
+		usage = usage + temp_memory.usage();
 }
 
 /*
